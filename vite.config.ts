316080f5faf096bf69fd36d91034ca0d1fca4cf9
wizard-cloud <myfredm21@gmail.com>
2026-07-29 13:50:06 -0600

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/workspace/", // 👈 nombre EXACTO del repo en GitHub
+  base: "/LandingMarmSolutions/", // 👈 nombre EXACTO del repo en GitHub
 });
